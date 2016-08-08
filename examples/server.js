97367f36de5d595955528150012dfcdf84bbaf51
goliatone <burgosemi@gmail.com>
2016-08-08 17:03:48 -0400

    exapmle: update server

--- examples/server.js
+++ examples/server.js
@@ -2,12 +2,25 @@ var REPL = require('../lib/repl');
 
 var repl = new REPL({
     root: __dirname,
-    package:{
+    metadata:{
         name: 'Test',
         version: '0.0.0',
         environment: 'development'
     },
-
-
+    context: {
+        app:{
+            logger:console,
+            mute: function(){
+                console.log('MUTE');
+            },
+            unmute: function(){
+                console.log('UNMUTE');
+            },
+            config:{
+                name: 'poke-repl'
+            }
+        }
+    }
 });
+
 repl.start();
